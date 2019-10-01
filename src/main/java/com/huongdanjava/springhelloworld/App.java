ad66c64ffe44bd670938fb8a3a01b1c1ab8c8ff2
Tuyen Nguyen <tnguyen495@dxc.com>
2019-10-01 14:57:17 +0700

    Add all my files

---
+++ src/main/java/com/huongdanjava/springhelloworld/App.java
@@ -0,0 +1,17 @@
+package com.huongdanjava.springhelloworld;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+	public static void main(String[] args) {
+		// Call Spring container
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+		// Request to get SayHello object
+		SayHello sayHello = (SayHello) context.getBean("hello");
+
+		// Call any methods in SayHello object
+		sayHello.say();
+	}
+}
---
+++ src/main/java/com/huongdanjava/springhelloworld/SayHello.java
@@ -0,0 +1,9 @@
+package com.huongdanjava.springhelloworld;
+
+public class SayHello {
+
+	public void say() {
+		System.out.println("Hello World!");
+	}
+	
+}
